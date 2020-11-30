01e2fc908cfe8de64dd65b3956422e8c22b01576
Pedro Victor Pontes Pinheiro <ppinheiro@banparanet.com.br>
2020-11-30 10:25:21 -0300

    Including the correct query for cherwell

--- database/database.go
+++ database/database.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	verifyQuerySQL                string = "select 1"
-	getNumberOfPriorityTasksQuery string = "select count(*) from cherwell where prioridade = 1"
+	getNumberOfPriorityTasksQuery string = "select count(*) from Incidente where OwnedByTeam = 'SUSIS - GERIN' and Prioridade in (1,2) and OwnerID = '' and Status = 'Encaminhado'"
 )
 
 var connection *sql.DB
